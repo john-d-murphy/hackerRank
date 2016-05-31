0995c4b7589a05d134ad081e685140201062515d
John Murphy <mr.proxxxy@gmail.com>
2016-05-31 19:32:37 -0400

    new bash is different than Solaris!

---
+++ Linux Shell/Bash/02.loopingAndSkipping.sh
@@ -0,0 +1,23 @@
+
+# - iterating between two integers, a and b
+# - iterating between two integers, a and b, and incrementing by c each time
+# - iterating through the elements of an array, etc.
+#
+# Your task is to use for loops to display only odd natural numbers from 1 to 99.
+#
+# Input
+# There is no input.
+#
+# Output
+#
+# 1
+# 3
+# 5
+# .
+# .
+# .
+# .
+# .
+# 99
+
+for i in {1..99..2}; do echo "$i"; done
