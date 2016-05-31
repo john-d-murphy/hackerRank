928c7858fe477ed309e1db0ba3b649fc06c5fbcc
John Murphy <mr.proxxxy@gmail.com>
2016-05-31 19:45:19 -0400

    same as problem 2

---
+++ Linux Shell/Bash/04.loopingWithNumbers.sh
@@ -0,0 +1,9 @@
+
+# for loops in Bash can be used in several ways:
+# - iterating between two integers, a and b
+# - iterating between two integers, a and b, and incrementing by c each time
+# - iterating through the elements of an array, etc.
+#
+# Use for loops to display the natural numbers from 1 to 50.
+
+for i in {1..50}; do echo "$i"; done
