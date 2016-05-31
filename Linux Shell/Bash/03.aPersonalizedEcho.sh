1686d8d20c69d1554cb7c9371481aeb19d9f5af8
John Murphy <mr.proxxxy@gmail.com>
2016-05-31 19:42:36 -0400

    Another simple bash exercise.

---
+++ Linux Shell/Bash/03.aPersonalizedEcho.sh
@@ -0,0 +1,25 @@
+
+# Write a Bash script which accepts  as input and displays a greeting: "Welcome (name)"
+#
+# Input Format
+# One line, containing a .
+#
+# Output Format
+# One line: "Welcome (name)" (quotation marks excluded).
+# The evaluation will be case-sensitive.
+#
+# Sample Input 0
+#
+# Dan
+# Sample Output 0
+#
+# Welcome Dan
+# Sample Input 1
+#
+# Prashant
+# Sample Output 1
+#
+# Welcome Prashant
+
+read name
+echo "Welcome $name"
