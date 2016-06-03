9e3c30483d709df81f4d55e338582fa1250c00cd
John Murphy <mr.proxxxy@gmail.com>
2016-06-02 20:26:45 -0400

    cut -b1-4

---
+++ Linux Shell/Text Processing/04.cut4.sh
@@ -0,0 +1,21 @@
+# Input Format
+# A text file with lines of ASCII text only.
+#
+# Input Constraints
+# 1 <= N <= 100 (N is the number of lines of text in the input file)
+#
+# Output Format
+# The output should contain N lines. Each line should contain just the first four characters of the corresponding input line.
+#
+# Sample Input
+#
+# Hello
+# World
+# how are you
+# Sample Output
+#
+# Hell
+# Worl
+# how
+
+cut -b1-4
