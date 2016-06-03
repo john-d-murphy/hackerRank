237619e72c6d2ea058e5701e4c3c4cc1d8ce9781
John Murphy <mr.proxxxy@gmail.com>
2016-06-02 20:48:13 -0400

    ehhhh - not great

---
+++ Linux Shell/Text Processing/12.middleOfATextFile.sh
@@ -0,0 +1,13 @@
+# Task
+#
+# Display the lines (from line number 12 to 22, both inclusive) of a given text file.
+#
+# Input Format
+#
+# A text file
+#
+# Output Format
+#
+# Display the lines (from line number 12 to 22, both inclusive) for the input file.
+
+head -n 22 | tail -n 11
