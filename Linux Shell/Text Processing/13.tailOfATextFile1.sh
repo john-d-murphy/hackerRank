15c35ebb5d222da72dd0a1307f14b26e27398c62
John Murphy <mr.proxxxy@gmail.com>
2016-06-03 06:19:00 -0400

    tail -20

---
+++ Linux Shell/Text Processing/13.tailOfATextFile1.sh
@@ -0,0 +1,30 @@
+# Problem
+# Submissions
+# Leaderboard
+# Discussions
+# Objective
+# In this challenge, we practice using the tail command to display the last  lines of a text file.
+#
+# Resources
+# Here's a useful video on the topic:
+#
+#
+# By default, it displays the last n lines when used in the following form:
+#
+# tail [filename]
+# tail -n 11 [filename]  -> Last 11 lines
+# tail -c 20 [filename]  -> Last 20 characters
+# We can also specify a certain number of lines to be displayed and list multiple filenames as well. It may also be used to display a specified number of bytes from an input file. Click here for more details about using the tail command.
+#
+# Task
+# Display the last 20 lines of an input file.
+#
+# Input Format
+#
+# A text file.
+#
+# Output Format
+#
+# Output the last 20 lines of the text file.
+
+tail -20
