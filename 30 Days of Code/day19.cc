4d880acbfbba8deb95368ad593402d996a17f18a
John Murphy <mr.proxxxy@gmail.com>
2016-06-11 15:44:22 -0400

    put 19 in the wrong project ... simple execution of an interface.

---
+++ 30 Days of Code/day19.cc
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+/*
+Objective
+Today, we're learning about Interfaces.
+Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+The AdvancedArithmetic interface and the method declaration for the abstract int divisorSum(int n) method
+are provided for you in the editor below. Write the Calculator class, which implements the AdvancedArithmetic
+interface. The implementation for the divisorSum method must be public and take an integer parameter,n, and return
+the sum of all its divisors.
+
+Note: Because we are writing multiple classes in the same file, do not use an access modifier (e.g.: public) in your
+class declaration (or your code will not compile); however, you must use the public access modifier before your method
+declaration for it to be accessible by the other classes in the file.
+
+Input Format
+
+A single line containing an integer, n.
+
+Constraints
+1 <= n <= 1000
+
+Output Format
+
+You are not responsible for printing anything to stdout. The locked Solution class in the editor below will call
+your code and print the necessary output.
+
+Sample Input
+6
+
+Sample Output
+I implemented: AdvancedArithmetic
+12
+*/
+
+class AdvancedArithmetic {
+public:
+  virtual int divisorSum(int n)=0;
+};
+
+class Calculator : public AdvancedArithmetic {
+  int divisorSum(int n) {
+    int sum = 0;
+    for (int i = 1 ; i <= n ; i++ ) {
+      if (n % i == 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+};
+
+int main(){
+  int n;
+  cin >> n;
+  AdvancedArithmetic *myCalculator = new Calculator();
+  int sum = myCalculator->divisorSum(n);
+  cout << "I implemented: AdvancedArithmetic\n" << sum;
+  return 0;
+}
