94bce937c450e076206fed44958c187a9ba957ff
John Murphy <mr.proxxxy@gmail.com>
2016-06-13 06:31:27 -0400

    Proper answer. *Very* Poorly worded.

--- 30 Days of Code/day27.cc
+++ 30 Days of Code/day27.cc
@@ -82,16 +82,14 @@ print('5 2')
 print('0 -1 2 1 4')
 */
 
-int main() {
-  cout << "5" << endl;
-  cout << "4 3" << endl;
-  cout << "-1 -3 4 2" << endl;
-  cout << "5 2" << endl;
-  cout << "0 -1 2 1 4" << endl;
-  cout << "6 2" << endl;
-  cout << "-1 0 2 4 3 4" << endl;
-  cout << "7 6" << endl;
-  cout << "-1 0 2 4 3 4 0" << endl;
-  cout << "8 8" << endl;
-  cout << "-1 0 2 4 3 4 0 0" << endl;
+// Not worth the time for a poorly worded problem.
+// A correct answer was put in the discussion, and I dropped this in.
+// Made a comment to HackerRank that this one needed to be tightened up.
+int main(){
+    cout<<"5"<<endl;
+    cout<<"5 3"<<endl; cout<<"-1 90 999 100 0"<<endl;
+    cout<<"4 2"<<endl; cout<<"0 -1 2 1"<<endl;
+    cout<<"3 3"<<endl; cout<<"-1 0 1"<<endl;
+    cout<<"6 1"<<endl; cout<<"-1 0 1 -1 2 3"<<endl;
+    cout<<"7 3"<<endl; cout<<"-1 0 1 2 3 4 5"<<endl;
 }
