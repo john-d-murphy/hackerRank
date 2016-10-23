ae4646ea56c5f45a9f9a740ed5f912164411ebe6
John Murphy <mr.proxxxy@gmail.com>
2016-10-23 11:42:14 -0400

    Version that passes three, but times out. Need to use heap instead of singly linked list.

--- Cracking The Coding Interview/Java/08.Heaps.FindTheRunningMedian.java
+++ Cracking The Coding Interview/Java/08.Heaps.FindTheRunningMedian.java
@@ -38,13 +38,75 @@ double-precision number scaled to decimal place (e.g., 1.2 format).
 
 public class Solution {
 
+  private static class Node {
+    public int value;
+    public Node next;
+
+    Node(int value) {
+      this.value = value;
+      next = null;
+    }
+  }
+
+  private static Node listInsert(Node toInsert, Node root) {
+    if (root == null) {
+      return toInsert;
+    } else if (toInsert.value < root.value) {
+      toInsert.next = root;
+      return toInsert;
+    } else {
+      root.next = listInsert(toInsert, root.next);
+      return root;
+    }
+  }
+
+  private static double getMedian(int index, boolean getNext, Node root) {
+    Node tempNode = root;
+    double median;
+    //System.out.printf("Index: %s - Get Next: %s\n", index, getNext, root);
+
+    for( int i = 0 ; i < index; i++) {
+      tempNode = tempNode.next;
+    }
+
+    if (getNext) {
+      median = (tempNode.value + tempNode.next.value)/2.0;
+    } 
+    else {
+      median = tempNode.value;
+    }
+    return median;
+
+
+  }
+
+  private static void printList(Node root) {
+    Node tempRoot = root;
+    System.out.printf("List: ");
+    while( tempRoot != null) {
+      System.out.printf("%d ", tempRoot.value);
+      tempRoot = tempRoot.next;
+    }
+    System.out.printf("\n");
+  }
+
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int a[] = new int[n];
-    for(int a_i=0; a_i < n; a_i++){
-      a[a_i] = in.nextInt();
+    Node list = null;
+    for(int count=0; count < n; count++){
+      int value = in.nextInt();
+      Node newNode = new Node(value);
+      list = listInsert(newNode, list);
+      //printList(list);
+      int index = count / 2;
+      boolean getNext = count % 2 == 1;
+      //System.out.printf("Median Index: %d\n", index);
+      //System.out.printf("Get Next: %s\n", getNext);
+      double median = getMedian( index, getNext, list);
+      //System.out.printf("Median: %.2f\n##############\n", median);
+      System.out.printf("%.1f\n", median);
     }
   }
 }
