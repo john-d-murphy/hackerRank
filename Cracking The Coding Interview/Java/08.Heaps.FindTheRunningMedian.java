43254bb88b32bdfe1c8b53bceb604b7f2186b7ab
John Murphy <mr.proxxxy@gmail.com>
2016-10-23 14:56:21 -0400

    Whitespace cleanup

--- Cracking The Coding Interview/Java/08.Heaps.FindTheRunningMedian.java
+++ Cracking The Coding Interview/Java/08.Heaps.FindTheRunningMedian.java
@@ -5,23 +5,23 @@ import java.math.*;
 import java.util.regex.*;
 
 /*
-The median of a dataset of integers is the midpoint value of the dataset for which an equal number of integers 
-are less than and greater than the value. To find the median, you must first sort your dataset of integers in 
+The median of a dataset of integers is the midpoint value of the dataset for which an equal number of integers
+are less than and greater than the value. To find the median, you must first sort your dataset of integers in
 non-decreasing order, then:
 
-If your dataset contains an odd number of elements, the median is the middle element of the sorted sample. 
+If your dataset contains an odd number of elements, the median is the middle element of the sorted sample.
 In the sorted dataset {1,2,3}, 2 is the median.
-If your dataset contains an even number of elements, the median is the average of the two middle elements of 
+If your dataset contains an even number of elements, the median is the average of the two middle elements of
 the sorted sample. In the sorted dataset {1,2,3,4}, (2 + 3)/2 = 2.5 is the median.
 Given an input stream of n integers, you must perform the following task for each ith integer:
 
 Add the ith integer to a running list of integers.
 Find the median of the updated list (i.e., for the first element through the ith element).
-Print the list's updated median on a new line. 
+Print the list's updated median on a new line.
 The printed value must be a double-precision number scaled to decimal place (e.g., 1.2 format).
 
 Input Format
-The first line contains a single integer,n, denoting the number of integers in the data stream. 
+The first line contains a single integer,n, denoting the number of integers in the data stream.
 Each line t of the n subsequent lines contains an integer,ai, to be added to your list.
 
 Constraints
@@ -31,10 +31,9 @@ Constraints
 
 Output Format
 
-After each new integer is added to the list, print the list's updated median on a new line as a single 
+After each new integer is added to the list, print the list's updated median on a new line as a single
 double-precision number scaled to decimal place (e.g., 1.2 format).
 */
-
 
 public class Solution {
 
@@ -71,12 +70,11 @@ public class Solution {
 
     if (getNext) {
       median = (tempNode.value + tempNode.next.value)/2.0;
-    } 
+    }
     else {
       median = tempNode.value;
     }
     return median;
-
 
   }
 
