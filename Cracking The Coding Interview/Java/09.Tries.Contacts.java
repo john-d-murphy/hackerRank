e54c06c0d205d13ceb4934fa62206e21a7a43fbd
John Murphy <mr.proxxxy@gmail.com>
2016-10-23 23:25:30 -0400

    Initial commit of problem specification

---
+++ Cracking The Coding Interview/Java/09.Tries.Contacts.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/*
+We're going to make our own Contacts application! The application must perform two types of operations:
+
+1. add name, where  is a string denoting a contact name. This must store name as a new contact in the application.
+2. find partial, where partial is a string denoting a partial name to search the application for.
+It must count the number of contacts starting with partial and print the count on a new line.
+Given n sequential add and find operations, perform each operation in order.
+
+Input Format
+
+The first line contains a single integer,n, denoting the number of operations to perform.
+Each line i of the n subsequent lines contains an operation in one of the two forms defined above.
+
+Constraints
+* 1 <= n <= 10^5
+* 1 <= |name| <= 21
+* 1 <= |partial <= 21
+* it is guaranteed that name and partial contain lowercase English letters only
+
+Output Format
+
+For each find partial operation, print the number of contact names starting with partial on a new line.
+
+Sample Input
+
+4
+add hack
+add hackerrank
+find hac
+find hak
+Sample Output
+
+2
+0
+Explanation
+
+We perform the following sequence of operations:
+
+Add a contact named hack.
+
+Add a contact named hackerrank.
+
+Find and print the number of contact names beginning with hac.
+There are currently two contact names in the application and both of them start with hac, so we print 2 on a new line.
+
+Find and print the number of contact names beginning with hak.
+There are currently two contact names in the application but neither of them start with hak, so we print 0
+on a new line.
+*/
