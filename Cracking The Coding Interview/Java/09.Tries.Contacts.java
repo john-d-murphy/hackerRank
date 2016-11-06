0de855b7bdbe85ff9e7b05f24c8c2fb1606dee3e
John Murphy <mr.proxxxy@gmail.com>
2016-11-06 10:29:02 -0500

    Boilerplate

--- Cracking The Coding Interview/Java/09.Tries.Contacts.java
+++ Cracking The Coding Interview/Java/09.Tries.Contacts.java
@@ -7,7 +7,7 @@ import java.util.regex.*;
 /*
 We're going to make our own Contacts application! The application must perform two types of operations:
 
-1. add name, where  is a string denoting a contact name. This must store name as a new contact in the application.
+1. add name, where n is a string denoting a contact name. This must store name as a new contact in the application.
 2. find partial, where partial is a string denoting a partial name to search the application for.
 It must count the number of contacts starting with partial and print the count on a new line.
 Given n sequential add and find operations, perform each operation in order.
@@ -53,3 +53,15 @@ Find and print the number of contact names beginning with hak.
 There are currently two contact names in the application but neither of them start with hak, so we print 0
 on a new line.
 */
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++){
+            String op = in.next();
+            String contact = in.next();
+        }
+    }
+}
