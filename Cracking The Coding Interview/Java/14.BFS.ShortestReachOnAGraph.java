5b80a70d5c919a91e80137200117284cc10c6004
John Murphy <mr.proxxxy@gmail.com>
2016-12-26 13:22:41 -0500

    Adding Simple Solution for BFS

--- Cracking The Coding Interview/Java/14.BFS.ShortestReachOnAGraph.java
+++ Cracking The Coding Interview/Java/14.BFS.ShortestReachOnAGraph.java
@@ -15,7 +15,7 @@ if s is disconnected from a node, print -1 as the distance to that node.
 
 Input Format
 
-The first line contains an integer,q, denoting the number of queries.
+The first line contains an integer, q, denoting the number of queries.
 The subsequent lines describe each query in the following format:
 
 The first line contains two space-separated integers describing the respective values of n (the number of nodes) and
@@ -24,7 +24,7 @@ m (the number of edges) in the graph.
 Each line i of the m subsequent lines contains two space-separated integers, u and v, describing an edge connecting
 node u to node v.
 
-The last line contains a single integer,s, denoting the index of the starting node.
+The last line contains a single integer, s, denoting the index of the starting node.
 
 Constraints
 
@@ -54,19 +54,49 @@ Sample Output
 -1 6
 */
 
+public class Solution {
 
-public class Solution  {
-  public satic class Graph {
+  public static class Graph {
+
+    Map<Integer, Set<Integer>> localGraph;
+
     public Graph(int size) {
-
-
+      localGraph = new HashMap<Integer, Set<Integer>>();
+      for(int i = 1; i <= size; i++) {
+        localGraph.put(i, new HashSet<Integer>());
+      }
     }
 
     public void addEdge(int first, int second) {
+      localGraph.get(first).add(second);
+      localGraph.get(second).add(first);
     }
 
-    //0 indexed
     public int[] shortestReach(int startId) {
+      int[] distances = new int[localGraph.size()+1];
+      Arrays.fill(distances, -1);
+
+      Set<Integer> visitedNodes   = new HashSet<Integer>();
+      Queue<Integer> nodesToVisit = new LinkedList<Integer>();
+      nodesToVisit.add(startId);
+
+      distances[startId] = 0;
+
+      while( nodesToVisit.size() > 0) {
+
+        Integer currentNode = nodesToVisit.poll();
+        visitedNodes.add(currentNode);
+
+        for( Integer i : localGraph.get(currentNode)) {
+          if( !visitedNodes.contains(i)){
+            nodesToVisit.add(i);
+            visitedNodes.add(i);
+            distances[i] = distances[currentNode] + 1;
+          }
+        }
+      }
+
+      return distances;
     }
   }
 
@@ -75,34 +105,36 @@ public class Solution  {
 
     int queries = scanner.nextInt();
 
-    for (int t = 0; t < queries; t++){
-      //Create a graph of size n where each edge weight is 6
-      Graph graph  =  new Graph(scanner.nextInt());
-      int m = scanner.nextInt();
+    for (int query = 0;  query < queries; query++){
+      //Create the graph
+      Integer edges = scanner.nextInt();
+      Graph graph   = new Graph(edges);
+      int m         = scanner.nextInt();
 
       // read and set edges
       for( int i = 0; i < m; i++) {
-        int u = scanner.nextInt();
-        int v = Scanner.nextInt();
+        int first  = scanner.nextInt();
+        int second = scanner.nextInt();
 
         // add each edge to the graph
-        graph.addEdge(u,v);
+        graph.addEdge(first,second);
       }
 
       // Find shortest reach from node s
       int startId = scanner.nextInt();
       int[] distances = graph.shortestReach(startId);
 
-      for( int i = 0; i < distances.length ; i++){
-        if( i != startId) {
-          System.out.println(distances[i]);
-          System.out.print(" ");
+      for( int i = 1; i < distances.length ; i++){
+        if( i != startId && distances[i] > -1) {
+          System.out.printf("%d ",distances[i] * 6);
+        } else if( i != startId && distances[i] == -1) {
+          System.out.printf("%d ",distances[i]);
         }
       }
 
-      System.out.println();
-
+      System.out.printf("\n");
     }
-    Scanner.close();
+
+    scanner.close();
   }
 }
